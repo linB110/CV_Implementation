14d3972069e823eb1a890ac8d2df318a9992e17d
linB110 <a0905266518@gmail.com>
2025-06-04 15:32:01 +0800

    first commit

---
+++ keypoint/Tomasi.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2 as cv
+
+img = cv.imread('box.jpg')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# detect corners
+corners = cv.goodFeaturesToTrack(gray, 50, 0.01, 10)  # 50-> upper limit of corners
+corners = corners.astype(int)
+
+# plotting corners
+for i in corners:
+    x, y = i.ravel()
+    cv.circle(img, (x, y), 3, (0, 0, 255), -1)  # use red dot
+
+# store img and show result
+cv.imshow('Tomasi', img)
+cv.imwrite('Tomasi.jpg', img)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
+
