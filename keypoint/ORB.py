8ad7275ea390d5308716c700f77869ba830ab848
linB110 <a0905266518@gmail.com>
2025-06-04 19:41:45 +0800

    finished ORB implementation

---
+++ keypoint/ORB.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2 as cv
+
+img = cv.imread('box.jpg', cv.IMREAD_GRAYSCALE)
+
+# Initiate ORB detector
+orb = cv.ORB_create()
+
+# find the keypoints with ORB
+kp = orb.detect(img,None)
+
+# compute the descriptors with ORB
+kp, des = orb.compute(img, kp)
+
+# draw only keypoints location,not size and orientation
+img = cv.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
+
+# store processed img
+cv.imwrite('ORB.jpg', img)
+
+cv.imshow('ORB', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
