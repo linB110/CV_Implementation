9a063e737ad71a4f687005c71849cc8bb3ffb6d5
linB110 <a0905266518@gmail.com>
2025-06-04 19:14:14 +0800

    finished FAST implementation

---
+++ keypoint/FAST.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2 as cv
+
+img = cv.imread('box.jpg')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+fast = cv.FastFeatureDetector_create()
+
+# find and draw keypoints
+kp = fast.detect(img, None)
+img = cv.drawKeypoints(img, kp, None, color = (255, 0, 0))
+
+# store processed img
+cv.imwrite('FAST.jpg', img)
+
+# show result
+cv.imshow('FAST', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
