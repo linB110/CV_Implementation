937ca0d75e678857616eee4ab418dfee455be59c
Lin Huang Ting <138973404+linB110@users.noreply.github.com>
2026-02-11 08:38:10 +0800

    Update DBSCAN.py

--- clustering/DBSCAN.py
+++ clustering/DBSCAN.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 DEPTH_THRESHOLD = 10.0  # meters
 
-# read assocaited RGB + Depth
+# read associated RGB + Depth
 depth = cv2.imread("depth/1341846313.592088.png", cv2.IMREAD_UNCHANGED).astype(np.float32) / 5000.0
 rgb = cv2.imread("rgb/1341846313.592026.png")
 
@@ -22,7 +22,6 @@ fy = 539.2
 cx = 320.1
 cy = 247.6
 
-
 # pixel to point cloud
 x3d = (xs - cx) * depth[ys, xs] / fx
 y3d = (ys - cy) * depth[ys, xs] / fy
@@ -30,25 +29,37 @@ z3d = depth[ys, xs]
 
 points_3d = np.column_stack([x3d, y3d, z3d])
 
-scaler = StandardScaler()
-points_scaled = scaler.fit_transform(points_3d)
+points_scaled = np.column_stack([
+    x3d,
+    y3d,
+    z3d * 1.1   # weighted depth value
+])
 
-# KMeans clustering
-kmeans = KMeans(n_clusters=12, random_state=42, n_init=10)
-labels = kmeans.fit_predict(points_scaled)
+# DBSCAN clustering
+dbscan = DBSCAN(
+    eps = 0.03,
+    min_samples=50   # minimum samples to form a group
+)
+labels = dbscan.fit_predict(points_scaled)
 
-# impose to original RGB
-out = rgb.copy()
 unique_labels = np.unique(labels)
+print(f"Clusters found: {len(unique_labels) - (1 if -1 in unique_labels else 0)}")
+
+# visualization
+out = rgb.copy()
 colors = plt.cm.tab20(np.linspace(0, 1, len(unique_labels)))
 
 for i, lab in enumerate(unique_labels):
-    if np.sum(labels == lab) < 50:  # denoise
-        continue
+    if lab == -1:
+        continue  # denoise
+    
     mask_lab = labels == lab
+    
+    if np.sum(mask_lab) < 50:
+        continue
+    
     color = (colors[i][:3] * 255).astype(np.uint8)
     out[ys[mask_lab], xs[mask_lab]] = color
 
-cv2.imwrite("cluster_3d_kmeans.png", out)
-print(f"3D Clusters: {len(unique_labels)}")
-print("Saved: cluster_3d_kmeans.png")
+cv2.imwrite("cluster_3d_dbscan.png", out)
+print("Saved: cluster_3d_dbscan.png")
